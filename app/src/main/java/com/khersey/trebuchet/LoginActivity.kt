faab15bd635b0c84169d74c26908d3149d1a57cc
khersey <kmhersey951@gmail.com>
2017-09-27 16:34:29 -0400

    about to change over to clean-mvp, dont want to ruin everything

---
+++ app/src/main/java/com/khersey/trebuchet/LoginActivity.kt
@@ -0,0 +1,16 @@
+package com.khersey.trebuchet
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+/**
+ * Created by khersey on 2017-09-18.
+ */
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+
+    }
+}
