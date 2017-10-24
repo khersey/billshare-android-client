878c836434a1e4fbafe5eb020b10c2ae3ab33b93
Deyvid <deyvidmitkov@gmail.com>
2017-10-24 10:34:32 -0400

    added transactionCreator and inner transaction class

--- app/src/main/java/com/cleganeBowl2k18/trebuchet/data/entity/TransactionCreator.kt
+++ app/src/main/java/com/cleganeBowl2k18/trebuchet/data/entity/TransactionCreator.kt
@@ -1,7 +1,24 @@
 package com.cleganeBowl2k18.trebuchet.data.entity
 
-/**
- * Created by khersey on 2017-10-23.
- */
+import com.google.gson.annotations.SerializedName
+
 class TransactionCreator {
+    var total: Long = 0
+    @SerializedName("currency_code")
+    var currencyCode: String? = null
+    var label: String? = null
+    var group: Long? = null
+    var creator: Long? = null
+    var transactions: List<TransactionCreatorTransaction>? = null
+
+    constructor()
+
+    constructor(total: Long, currency_code: String, label: String, group: Long, creator: Long, transactions: List<TransactionCreatorTransaction>) {
+        this.total = total
+        this.currencyCode = currency_code
+        this.label = label
+        this.group = group
+        this.creator = creator
+        this.transactions = transactions
+    }
 }
